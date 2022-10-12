fa2b83dbb5b893fe951405476d15e6b8852be0c8
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-10-12 13:51:28 +0200

    added rle encoder and  decoder

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import os
+import time
+import pandas as pd
+from transformers.RLE import *
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/transformers/RLE.py
@@ -0,0 +1,70 @@
+from os import makedirs
+
+class RLE():
+    def __init__(self):
+        pass
+
+    def encode(self,data_path, res_f_name):
+        """ encoded strings are of type: string, start_idx, end_idx
+            where start_idx represents the starting index (starting at 0)
+            and end_idx represents the last index, (not to be included in the range)
+        """
+        # open file
+        with open(data_path, 'r') as f:
+            lines = f.readlines()
+        print(f'Number of lines to encode: {len(lines)}')
+
+        # reset output file
+        with open(res_f_name, 'w') as res:
+            pass
+
+        i = 0  # start index
+        done = False  # end condition
+        while not done:
+            pairs = 1
+            for j in lines[i+1:]:
+                if lines[i] == j:
+                    pairs += 1
+                else:
+                    break
+            
+            # write current res
+            res_str = f'{lines[i].strip()}, {i}, {i+pairs}\n'
+            with open(res_f_name, 'a') as res:
+                res.write(res_str)
+            
+            # increment counter and break if done
+            i += pairs
+            if i >= len(lines):
+                break
+        # return number of lines
+        with open(res_f_name, 'r') as res:
+            res_len = len(res.readlines())
+        return res_len
+
+
+    def decode(self, file_path, res_f_name):
+        
+        # reset output file
+        with open(res_f_name, 'w') as res:
+            pass
+
+        # open file
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+        print(f'Number of lines to decode: {len(lines)}')
+
+        for line in lines:
+            elems = line.strip().split(sep=',')
+            reps = int(elems[2]) - int(elems[1])
+            for i in range(reps):
+                with open(res_f_name, 'a') as res:
+                    res.write(elems[0]+'\n')
+
+        with open(res_f_name, 'r') as res:
+            res_len = len(res.readlines())
+        return res_len
+        
+
+
+

290c7debce0a60c997aa0a5b2fa8a2f730779560
“Thomas <thomasmaliappis@gmail.com>
2022-10-31 15:38:24 +0200

    binary DIC

--- src/solvers/DIC.py
+++ src/solvers/DIC.py
@@ -1,24 +1,20 @@
-class DIC():
-    def __init__(self, data_type):
-        self.name = "DIC"
-        self.extension = '.dic'
+import numpy as np
+from solvers.encoder_decoder import EncoderDecoder
+class DIC(EncoderDecoder):
+    def __init__(self, data_type: str) -> None:
+        super().__init__("DIC", '.dic', data_type)
 
     def encode(self, file_path, data_type='int8', res_dir=''):
-        """ encoded strings are of type: string, start_idx, end_idx
-            where start_idx represents the starting index (starting at 0)
-            and end_idx represents the last index, (not to be included in the range)
-
-            Params:
-            - file_path: path of the file to encode
-            - res_f_name: path + name to the output file
-        """
-        # open file
-        with open(file_path, 'r') as f:
-            lines = f.readlines()
+        
+        if self.data_type == 'string':
+            with open(file_path, 'r') as f:
+                lines = f.readlines()
+        else:
+            lines = np.fromfile(file_path, dtype=self.data_type, sep='\n')
+        
         print(f'Encoding {len(lines)} lines.')
 
-        # define output file name
-        res_f_name = res_dir+file_path.split('/')[-1]+self.extension
+        file_out = open(self.enc_file_path(file_path, res_dir), 'wb')
 
         # create keys
         keys = {}
@@ -28,16 +24,25 @@ class DIC():
                 keys[line] = counter
                 counter += 1
 
-        # create array of dictionary keys
-        idxes = ''
-        for line in lines:
-            idxes+=str(keys[line])+','
+        max_length = np.max(len(lines))
+        max_length_bytes = np.max([self.min_bytes_for(max_length), self.min_bytes_for(counter)])
 
-        # write results to file
-        with open(res_f_name, 'w') as res:
-            for key, item in keys.items():
-                res.write(f"{key[:-1]}|{item}\n")
-            res.write(idxes)
+        # write dict size
+        file_out.write(self.byte(int(max_length_bytes)))
+        file_out.write(self.byte(counter, max_length_bytes))
+        # write dict items to file
+        for key, value in keys.items():
+            if self.data_type == 'string':
+                file_out.write(key.encode('utf-8'))
+                file_out.write(self.byte(int(value), max_length_bytes))
+            else:
+                file_out.write(self.byte(int(key)))
+                file_out.write(self.byte(int(value)))
+
+        new_lines = [keys[line] for line in lines]
+        # write new values to file
+        for line in new_lines:
+                file_out.write(self.byte(int(line), max_length_bytes))
         
         return 0
 
@@ -47,34 +52,39 @@ class DIC():
             - file_path: path of the file to decode
             - res_f_name: path + name to the output file
         """
-        # open file
-        with open(file_path, 'r') as f:
-            lines = f.readlines()
-        # print(f'Decoding {len(lines)} lines.')
 
-        # define output file name
-        res_f_name = res_dir+file_path.split('/')[-1]+'.csv'
-        
-        # create dict
+        file_in = open(file_path, 'rb')
+        file_out = open(self.dec_file_path(file_path, res_dir), 'w')
+
+        max_length_bytes = self.number(file_in.read(self.byte_len))
+        dict_size = self.number(file_in.read(max_length_bytes))
         keys = {}
-        for line in lines[:-1]:
-            elem = line.split('|')
-            item = ''.join(elem[:-1])
-            key = int(elem[-1])
-            if key not in keys:
-                keys[key] = item
-        
-        # create my idxes
-        idxes = lines[-1].strip().split(',')
-        idxes = [int(i) for i in idxes[:-1]]
-        
-        # create output string
-        res_str = ''
-        for i in idxes:
-            res_str += keys[i]+'\n'
+        for _ in range(dict_size):
+            if self.data_type == 'string':
+                key_bytes = file_in.readline()[:-1]
+                key = key_bytes.decode('utf-8')
+                value_bytes = file_in.read(max_length_bytes)
+                value = self.number(value_bytes)
+            else:
+                key_bytes = file_in.read(self.byte_len)
+                key = self.number(key_bytes)
+                value_bytes = file_in.read(self.byte_len)
+                value = self.number(value_bytes)
+            keys[value] = key
 
-        # write our results to file
-        with open(res_f_name, 'w') as res:
-            res.write(res_str)
+        data_bytes = file_in.read(max_length_bytes)
+        lines = []
+        while data_bytes:
+            lines.append(self.number(data_bytes))
+            data_bytes = file_in.read(max_length_bytes)
 
-        return res_str
+        data = ''
+        for line in lines:
+            if self.data_type == 'string':
+                original_line_value = keys[line]+'\n'
+            else:
+                original_line_value = str(keys[line])+'\n'
+            data += original_line_value
+            file_out.write(original_line_value)
+            
+        return data
